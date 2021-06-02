6f7a54db97e460e5015e80d90811f7b1be6fd36d
Connor Hood <connor_hood@yahoo.com>
2021-06-02 16:15:15 -0500

    added get and create endpoints, tested works in postman

--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -13,3 +13,10 @@ class CardList(APIView):
         card = Card.objects.all()
         serializer = CardSerializer(card, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
