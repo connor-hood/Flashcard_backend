8a4909d5de9a00ad2584174339b4567b39739f76
Connor Hood <connor_hood@yahoo.com>
2021-06-02 15:58:05 -0500

    almost done with API

--- flashcardproject/flashcardApp/models.py
+++ flashcardproject/flashcardApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Card(models.Model):
+    language = models.CharField(max_length=20)
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=100)
     card_count = models.IntegerField()
--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Card
+from .serializers import CardSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class CardList(APIView):
+
+    def get(self, request):
+        card = Card.objects.all()
+        serializer = CardSerializer(card, many=True)
+        return Response(serializer.data)
