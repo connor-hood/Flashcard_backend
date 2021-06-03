2d9dfc1abc3baba3d8b5a4194ead29c2acd995dc
Connor Hood <connor_hood@yahoo.com>
2021-06-03 16:05:49 -0500

    more refactoring

--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -1,37 +1,14 @@
-from django.shortcuts import render
 from .models import Card
 from .serializers import CardSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import generics
 
 
 # Create your views here.
-class CardList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class CardList(generics.ListCreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CardDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class CardDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
