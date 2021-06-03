7f25ab696e8cfde60a203b36f484972cab0f697d
Connor Hood <connor_hood@yahoo.com>
2021-06-03 15:14:20 -0500

    more work, refactored code per REST tutorial

--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -3,47 +3,35 @@ from .models import Card
 from .serializers import CardSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, mixins, generics
 
 
 # Create your views here.
-class CardList(APIView):
+class CardList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
 
-    def get(self, request):
-        card = Card.objects.all()
-        serializer = CardSerializer(card, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        serializer = CardSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CardDetail(APIView):
+class CardDetail(mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
 
-    def get_object(self, pk):
-        try:
-            return Card.objects.get(pk=pk)
-        except Card.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk):
-        card = self.get_object(pk)
-        serializer = CardSerializer(card)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        card = self.get_object(pk)
-        serializer = CardSerializer(card, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        card = self.get_object(pk)
-        card.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
