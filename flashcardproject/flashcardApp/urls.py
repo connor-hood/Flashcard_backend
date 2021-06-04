a3593798cbb4c9fded68e42904d1ef9774031605
Connor Hood <connor_hood@yahoo.com>
2021-06-04 13:08:52 -0500

    almost done with back end, need to figure out how to view cards based on collection ID

--- flashcardproject/flashcardApp/urls.py
+++ flashcardproject/flashcardApp/urls.py
@@ -2,11 +2,34 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from django.views.generic import RedirectView
+from .views import CardViewSet, CollectionViewSet, api_root
+from rest_framework import renderers
 
-urlpatterns = [
-    path('cards/', views.CardList.as_view()),
-    path('', RedirectView.as_view(url='cards/')),
-    path('cards/<int:pk>/', views.CardDetail.as_view()),
-]
+card_list = CardViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+card_detail = CardViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+card_highlight = CardViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+collection_list = CollectionViewSet.as_view({
+    'get': 'list'
+})
+collection_detail = CollectionViewSet.as_view({
+    'get': 'retrieve'
+})
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('cards/', card_list, name='card-list'),
+    path('cards/<int:pk>/', card_detail, name='card-detail'),
+    path('collections/<int:pk>/cards/', card_highlight, name='card-highlight'),
+    path('collections/', collection_list, name='collection-list'),
+    path('collections/<int:pk>/', collection_detail, name='collection-detail')
+])
--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -1,18 +1,20 @@
-from .models import Card
-from .serializers import CardSerializer
-from rest_framework import generics, renderers
+from rest_framework import viewsets, permissions, renderers, generics
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from .serializers import CardSerializer, CollectionSerializer
+from .models import Card, Collection
 
 
 # Create your views here.
-class CardList(generics.ListCreateAPIView):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'collections': reverse('collection-list', request=request, format=format),
+        'cards': reverse('card-list', request=request, format=format)
+    })
 
-class CardDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
 
 class CardHighlight(generics.GenericAPIView):
     queryset = Card.objects.all()
@@ -21,3 +23,41 @@ class CardHighlight(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         card = self.get_object()
         return Response(card.highlighted)
+
+
+class CollectionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        card = self.get_object()
+        return Response(card.question)
+
+    def perform_create(self, serializer):
+        serializer.save()
+# class CardList(generics.ListCreateAPIView):
+#     queryset = Card.objects.all()
+#     serializer_class = CardSerializer
+#
+#
+# class CardDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Card.objects.all()
+#     serializer_class = CardSerializer
+#
+#
+# class CollectionList(generics.ListCreateAPIView):
+#     queryset = Collection.objects.all()
+#     serializer_class = CollectionSerializer
+#
+#
+# class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Collection.objects.all()
+#     serializer_class = CollectionSerializer
+#
+
