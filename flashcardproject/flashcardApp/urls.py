1c8473a99ad6d438c9ef2fff7e196a14a05c9d6c
Connor Hood <connor_hood@yahoo.com>
2021-06-07 15:02:48 -0500

    refactored back end

--- flashcardproject/flashcardApp/urls.py
+++ flashcardproject/flashcardApp/urls.py
@@ -2,34 +2,32 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from django.views.generic import RedirectView
-from .views import CardViewSet, CollectionViewSet, api_root
+
 from rest_framework import renderers
 
-card_list = CardViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-card_detail = CardViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-card_highlight = CardViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-collection_list = CollectionViewSet.as_view({
-    'get': 'list'
-})
-collection_detail = CollectionViewSet.as_view({
-    'get': 'retrieve'
-})
+# card_list = CardViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# card_detail = CardViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# card_highlight = CardViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# collection_list = CollectionViewSet.as_view({
+#     'get': 'list'
+# })
+
 
 urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('cards/', card_list, name='card-list'),
-    path('cards/<int:pk>/', card_detail, name='card-detail'),
-    path('collections/<int:pk>/cards/', card_highlight, name='card-highlight'),
-    path('collections/', collection_list, name='collection-list'),
-    path('collections/<int:pk>/', collection_detail, name='collection-detail')
+    path('', RedirectView.as_view(url='/collections/')),
+    path('cards/', views.CardList.as_view()),
+    path('cards/<int:pk>/', views.CardDetail.as_view()),
+    path('collections/<int:pk>/cards/', views.CollectionDetail.as_view()),
+    path('collections/', views.CollectionList.as_view()),
+    path('collections/<int:pk>/', views.CollectionList.as_view()),
 ])
--- flashcardproject/flashcardApp/views.py
+++ flashcardproject/flashcardApp/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, permissions, renderers, generics
-from rest_framework.decorators import action, api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from .serializers import CardSerializer, CollectionSerializer
@@ -8,56 +8,81 @@ from .models import Card, Collection
 
 # Create your views here.
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'collections': reverse('collection-list', request=request, format=format),
-        'cards': reverse('card-list', request=request, format=format)
-    })
+class CardDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Card.objects.get(pk=pk)
+        except Card.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        card = self.get_object(pk)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CardHighlight(generics.GenericAPIView):
-    queryset = Card.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
+class CardList(APIView):
+    def get(self, reqeust):
+        cards = Card.objects.all()
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
 
-    def get(self, request, *args, **kwargs):
-        card = self.get_object()
-        return Response(card.highlighted)
+    def post(self, request):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CollectionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Collection.objects.all()
-    serializer_class = CollectionSerializer
+class CollectionList(APIView):
+    def get(self, reqeust, pk):
+        collections = Collection.objects.all()
+        serializer = CollectionSerializer(collections, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CardViewSet(viewsets.ModelViewSet):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
+class CollectionDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Collection.objects.get(pk=pk)
+        except Collection.DoesNotExist:
+            raise Http404
 
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def highlight(self, request, *args, **kwargs):
-        card = self.get_object()
-        return Response(card.question)
+    def get(self, request, pk, format=None):
+        collection = self.get_object(pk)
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
 
-    def perform_create(self, serializer):
-        serializer.save()
-# class CardList(generics.ListCreateAPIView):
-#     queryset = Card.objects.all()
-#     serializer_class = CardSerializer
-#
-#
-# class CardDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Card.objects.all()
-#     serializer_class = CardSerializer
-#
-#
-# class CollectionList(generics.ListCreateAPIView):
-#     queryset = Collection.objects.all()
-#     serializer_class = CollectionSerializer
-#
-#
-# class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Collection.objects.all()
-#     serializer_class = CollectionSerializer
-#
+    def put(self, request, pk, format=None):
+        collection = self.get_object(pk)
+        serializer = CollectionSerializer(collection, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk, format=None):
+        collection = self.get_object(pk)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
